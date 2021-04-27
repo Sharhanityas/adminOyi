8659834af34264ad29ce8d06f35d4398c21484c7
Sharhani Qisthinaningtyas <63629490+Sharhanityas@users.noreply.github.com>
2021-04-27 15:39:35 +0700

    Add files via upload

--- database/migrations/2021_04_08_042134_create_speakers_table.php
+++ database/migrations/2021_04_08_042134_create_speakers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEventsTable extends Migration
+class CreateSpeakersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('events', function (Blueprint $table) {
+        Schema::create('speakers', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('judul_event');
-            $table->string('slug');
-            $table->string('tanggal_event');
-            $table->string('waktu');
-            $table->string('tempat');
+            $table->string('event_id');
+            $table->string('nama');
+            $table->string('email');
+            $table->string('telepon');
+            $table->string('foto');
             $table->longText('deskripsi');
 
             $table->softDeletes();
@@ -34,6 +34,6 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('speakers');
     }
 }
