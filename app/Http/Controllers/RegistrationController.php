8089cf723fe35c4967134d1db44e6da980839cd5
Sharhani Qisthinaningtyas <63629490+Sharhanityas@users.noreply.github.com>
2021-08-04 14:40:53 +0700

    Add files via upload

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+use App\Models\Registration;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Registration as IlluminateEvent;
+use Symfony\Component\EventDispatcher\Registration as SymfonyEvent;
+use App\Models\Event;
+
+class RegistrationController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Registration::with('event')->get();
+
+        return view('pages.Registrations.index')->with([
+            'items' => $items
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $events = Event::all();
+
+        return view('pages.Registrations.create')->with([
+            'events' => $events
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        Registration::create($data);
+        return redirect()->route('registration.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = Registration::findOrFail($id);
+
+        return view('pages.Registrations.editor')->with([
+
+            'item'  => $item
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $item = Registration::findOrFail($id);
+        $item->update($data);
+
+        return redirect()->route('registration.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Registration::findOrFail($id);
+        $item->delete();
+
+        Registration::where('event_id', $id)->delete();
+
+        return redirect()->route('registration.index');
+    }
+}
